326c62bb53e18747ad7338eaa4fb2f519e3e3e29
Folkor1 <igorvasiljevs@gmail.com>
2023-01-18 21:50:55 +0000

    Add dirs and media url to setting and home page structure

--- golden_coin/settings.py
+++ golden_coin/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'golden_coin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,3 +152,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
