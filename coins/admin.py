842f2b4276b66cd4e7ba3b1472b3e790e0fcf802
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 15:46:18 +0000

    Add metal and coins models to admin

--- coins/admin.py
+++ coins/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Metal, Coins
 
-# Register your models here.
+
+class CoinsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'metal',
+        'price',
+        'quantity',
+        'origin',
+        'year',
+        'condition',
+        'era',
+        'image',
+        'id',
+    )
+
+    ordering = ('metal',)
+
+
+class MetalAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Coins, CoinsAdmin)
+admin.site.register(Metal, MetalAdmin)
