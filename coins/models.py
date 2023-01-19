e8121fd8dda5d8d456eb60d8bd74b28d1f46a69c
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 15:45:49 +0000

    Add coins model

--- coins/models.py
+++ coins/models.py
@@ -16,3 +16,41 @@ class Metal(models.Model):
 
     class Meta:
         verbose_name_plural = 'Metals'
+
+
+CONDITION_CHOICES = (
+    ("uncirculated", "Uncirculated"),
+    ("fine", "Fine"),
+    ("good", "Good"),
+    ("fair", "Fair"),
+)
+
+ERA_CHOICES = (
+    ("ancient", "Ancient"),
+    ("medium", "Medium"),
+    ("modern", "Modern"),
+)
+
+
+class Coins(models.Model):
+    """
+    Class for Coins model
+    """
+    metal = models.ForeignKey('Metal', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    origin = models.CharField(max_length=254)
+    year = models.CharField(max_length=20)
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES, default=None)
+    era = models.CharField(max_length=20, choices=ERA_CHOICES, default=None)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Coins'
