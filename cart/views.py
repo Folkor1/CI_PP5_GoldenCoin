16fcf8b64d1a75ee0968cb13eda94d4208200417
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 19:53:06 +0000

    Add view and url for remove coins

--- cart/views.py
+++ cart/views.py
@@ -64,3 +64,23 @@ def adjust_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """
+    Remove the coin from the cart
+    """
+
+    try:
+        coins = get_object_or_404(Coins, pk=item_id)
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+        messages.success(request, f'Removed `{coins.name}` from the cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
