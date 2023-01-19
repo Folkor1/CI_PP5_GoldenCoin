5c6c719c64a47c6774556b76e9f4370fe8735986
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 15:42:39 +0000

    Add metal model

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Metal(models.Model):
+    """
+    Class for Metal model
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+    class Meta:
+        verbose_name_plural = 'Metals'
