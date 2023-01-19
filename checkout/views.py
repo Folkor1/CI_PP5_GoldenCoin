713a5f3b687e6b56ef3e149886a401e918afa2ed
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 20:29:25 +0000

    Add checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -72,12 +72,6 @@ def checkout(request):
 
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
 
     template = 'checkout/checkout.html'
     context = {
