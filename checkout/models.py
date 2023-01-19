db734d5de2b1fda36e3bc32c9e71e6c4e512bd92
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 20:11:48 +0000

    Add order model to checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,55 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from django_countries.fields import CountryField
+from coins.models import Coins
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    Class Order model
+    """
+
+    order_nr = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_nr = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(max_digits=20, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already
+        """
+        if not self.order_nr:
+            self.order_nr = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs
+        """
+        self.grand_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.save()
+
+    def __str__(self):
+        return self.order_nr
