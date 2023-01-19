33ac0fbc0415b8031717baea142dc0d1d2b4ed25
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 15:49:45 +0000

    Add view for all coins

--- coins/views.py
+++ coins/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Coins
 
-# Create your views here.
+
+def all_coins(request):
+    """
+    A view to show all coins
+    """
+    coins = Coins.objects.filter(quantity__gt=0)
+
+    context = {
+        'coins': coins,
+
+    }
+
+    return render(request, 'coins/coins.html', context)
