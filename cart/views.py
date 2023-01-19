810cf3cd8f9f4f14849fe75a8e54488e23deae40
Folkor1 <igorvasiljevs@gmail.com>
2023-01-19 19:35:18 +0000

    Add styles for increment/decrement menu

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 from coins.models import Coins
 
 
@@ -24,7 +25,6 @@ def add_to_cart(request, item_id):
     if item_id in list(cart.keys()):
         if int(item_id) in unique:
             cart[item_id] = 1
-            messages.info(request, f'Coin `{coins.name}` already added to cart')
         elif coin_quantity + cart[item_id] > coins.quantity:
             cart[item_id] = coins.quantity
             messages.error(request, f'Sorry, only {coins.quantity} `{coins.name}` are left in stock.')
