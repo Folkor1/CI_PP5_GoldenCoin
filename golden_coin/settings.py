70e4f238ff5ec3fbedd041b93b7c7e5b5d3d0d12
Folkor1 <igorvasiljevs@gmail.com>
2023-01-20 13:10:48 +0000

    Add stripe js script to handle payment form

--- golden_coin/settings.py
+++ golden_coin/settings.py
@@ -172,3 +172,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Stripe
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+DEFAULT_FROM_EMAIL = 'goldencoin@example.com'
