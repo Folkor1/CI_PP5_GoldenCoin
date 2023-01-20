e859afe28731fd17778a18b39ba29b5c68981630
Folkor1 <igorvasiljevs@gmail.com>
2023-01-20 13:22:18 +0000

    Add stripe keys to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -7,14 +7,16 @@ from .forms import OrderForm
 from django.views.decorators.http import require_POST
 from cart.contexts import cart_contents
 import json
+import stripe
 
 
 def checkout(request):
     """
     View the checkout page
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order_form = OrderForm()
     if request.method == 'POST':
         cart = request.session.get('cart', {})
 
@@ -74,10 +76,42 @@ def checkout(request):
 
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_nr': profile.default_phone_nr,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
