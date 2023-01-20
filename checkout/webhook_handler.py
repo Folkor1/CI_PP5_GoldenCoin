b9a832897992b9737f38c24a3e22560713ed3bde
Folkor1 <igorvasiljevs@gmail.com>
2023-01-20 16:01:08 +0000

    Add checkout config to apps

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,7 +10,9 @@ import time
 
 
 class StripeWH_Handler:
-    """Handle Stripe webhooks"""
+    """
+    Handle Stripe webhooks
+    """
 
     def __init__(self, request):
         self.request = request
