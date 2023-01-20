cb4cbf7a717ab3d3ced1d2244ce71d5c7061e5bc
Folkor1 <igorvasiljevs@gmail.com>
2023-01-20 22:49:37 +0000

    Add coins amount validation to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -19,104 +19,113 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
+    insufficient_coins = []
+    cart = request.session.get('cart', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_nr': request.POST['phone_nr'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
+    for i in cart.items():
+        coin_by_id = Coins.objects.get(id=i[0])
+        if coin_by_id.quantity < i[1]:
+            insufficient_coins.append(i[1])
+            messages.error(request, f'Sorry, only {coin_by_id.quantity} `{coin_by_id.name}` are left in stock. Please adjust the cart.')
+            return redirect(reverse('view_cart'))
+    else:
+        if request.method == 'POST':
+
+            form_data = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'phone_nr': request.POST['phone_nr'],
+                'country': request.POST['country'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'county': request.POST['county'],
+            }
+
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_cart = json.dumps(cart)
+                order.save()
+
+                for item_id, item_data in cart.items():
+                    try:
+                        coins = Coins.objects.get(id=item_id)
+                        coin = get_object_or_404(Coins, id=item_id)
+                        if isinstance(item_data, int):
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                coins=coins,
+                                coin_quantity=item_data,
+                            )
+                            coin.quantity = coin.quantity - item_data
+                            coin.save()
+                            order_line_item.save()
+                    except Coins.DoesNotExist:
+                        messages.error(request, (
+                            "One of the coins in cart wasn't found in our database."
+                            "Please call us for assistance!")
+                        )
+                        order.delete()
+                        return redirect(reverse('view_cart'))
+
+                # Save the info to the user's profile if all is well
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_nr]))
+            else:
+                messages.error(request, 'There was an error with your form. \
+                    Please double check your information.')
+
+        else:
+            cart = request.session.get('cart', {})
+            if not cart:
+                messages.error(request, "There's nothing in cart at the moment")
+                return redirect(reverse('coins'))
+
+            current_cart = cart_contents(request)
+            total = current_cart['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
+            # Attempt to prefill the form with any info the user maintains in their profile
+            if request.user.is_authenticated:
+                try:
+                    profile = UserProfile.objects.get(user=request.user)
+                    order_form = OrderForm(initial={
+                        'full_name': profile.user.get_full_name(),
+                        'email': profile.user.email,
+                        'phone_nr': profile.default_phone_nr,
+                        'country': profile.default_country,
+                        'postcode': profile.default_postcode,
+                        'town_or_city': profile.default_town_or_city,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'county': profile.default_county,
+                    })
+                except UserProfile.DoesNotExist:
+                    order_form = OrderForm()
+            else:
+                order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
         }
 
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
-
-            for item_id, item_data in cart.items():
-                try:
-                    coins = Coins.objects.get(id=item_id)
-                    coin = get_object_or_404(Coins, id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            coins=coins,
-                            coin_quantity=item_data,
-                        )
-                        coin.quantity = coin.quantity - item_data
-                        coin.save()
-                        order_line_item.save()
-                except Coins.DoesNotExist:
-                    messages.error(request, (
-                        "One of the coins in cart wasn't found in our database."
-                        "Please call us for assistance!")
-                    )
-                    order.delete()
-                    return redirect(reverse('view_cart'))
-
-            # Save the info to the user's profile if all is well
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_nr]))
-        else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
-
-    else:
-        cart = request.session.get('cart', {})
-        if not cart:
-            messages.error(request, "There's nothing in cart at the moment")
-            return redirect(reverse('coins'))
-
-        current_cart = cart_contents(request)
-        total = current_cart['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
-
-        # Attempt to prefill the form with any info the user maintains in their profile
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.user.email,
-                    'phone_nr': profile.default_phone_nr,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'county': profile.default_county,
-                })
-            except UserProfile.DoesNotExist:
-                order_form = OrderForm()
-        else:
-            order_form = OrderForm()
-
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
-
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
-
-    return render(request, template, context)
+        return render(request, template, context)
 
 
 def checkout_success(request, order_nr):
