2077efaba87a571ca380b0527cd7efffbaf6ca7f
Folkor1 <igorvasiljevs@gmail.com>
2023-01-22 13:16:41 +0000

    Add sell model

--- sell/models.py
+++ sell/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Sell(models.Model):
+    """
+    Class for Sell model
+    """
+
+    email = models.EmailField(max_length=70, null=True)
+    coin_name = models.CharField(max_length=50)
+    description = models.TextField()
+    metal = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    condition = models.CharField(max_length=100)
+    ask_price = models.DecimalField(max_digits=8, decimal_places=2)
+    negotiable = models.BooleanField(default=False)
+    image = models.ImageField(blank=False)
+
+    def __str__(self):
+        return self.coin_name
+
+    class Meta:
+        verbose_name_plural = 'Sell'
