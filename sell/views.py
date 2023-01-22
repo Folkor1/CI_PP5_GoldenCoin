30df8d7384172535e4d77d774e3acaed86fc1fb6
Folkor1 <igorvasiljevs@gmail.com>
2023-01-22 13:18:26 +0000

    Add view for sell app

--- sell/views.py
+++ sell/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .models import Sell
+from .forms import SellForm
 
-# Create your views here.
+
+def sell_coins(request):
+    """
+    Add coins to sell
+    """
+
+    if request.method == 'POST':
+        form = SellForm(request.POST, request.FILES)
+        if form.is_valid():
+            sell = form.save()
+            messages.success(request, 'We will review the offer and get in touch with you.')
+            return redirect(reverse('sell_coins'))
+        else:
+            messages.error(request, 'Failed to add coin.')
+    else:
+        form = SellForm()
+
+    template = 'sell/sell.html'
+    context = {
+        'form': form,
+        'on_sell_page': True
+    }
+
+    return render(request, template, context)
