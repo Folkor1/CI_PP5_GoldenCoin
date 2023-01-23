9aea72731527d855c6b1d779634f71673b10c313
Folkor1 <igorvasiljevs@gmail.com>
2023-01-23 21:52:54 +0000

    Add contact model to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Class for contact admin model
+    """
+    list_display = (
+        'email',
+        'name',
+        'buy_sell',
+        'phone_nr',
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
