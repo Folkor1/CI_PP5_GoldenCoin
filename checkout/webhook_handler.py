05294013d4c6daf9668c813298616b37f8d54622
Folkor1 <igorvasiljevs@gmail.com>
2023-01-24 22:17:12 +0000

    Fix validation for webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -77,47 +77,46 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-        if check():
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_nr = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
-            # Update profile information if save_info was checked
-            profile = None
-            username = intent.metadata.username
-            if username != 'AnonymousUser':
-                profile = UserProfile.objects.get(user__username=username)
-                if save_info:
-                    profile.default_phone_nr = shipping_details.phone
-                    profile.default_country = shipping_details.address.country
-                    profile.default_postcode = shipping_details.address.postal_code
-                    profile.default_town_or_city = shipping_details.address.city
-                    profile.default_street_address1 = shipping_details.address.line1
-                    profile.default_street_address2 = shipping_details.address.line2
-                    profile.default_county = shipping_details.address.state
-                    profile.save()
-                
-            order_exists = False
-            attempt = 1
-            while attempt <= 5:
-                try:
-                    order = Order.objects.get(
-                        full_name__iexact=shipping_details.name,
-                        email__iexact=billing_details.email,
-                        phone_nr__iexact=shipping_details.phone,
-                        country__iexact=shipping_details.address.country,
-                        postcode__iexact=shipping_details.address.postal_code,
-                        town_or_city__iexact=shipping_details.address.city,
-                        street_address1__iexact=shipping_details.address.line1,
-                        street_address2__iexact=shipping_details.address.line2,
-                        county__iexact=shipping_details.address.state,
-                        grand_total=grand_total,
-                        original_cart=cart,
-                        stripe_pid=pid,
-                    )
-                    order_exists = True
-                    break
-                except Order.DoesNotExist:
-                    attempt += 1
-                    time.sleep(1)
-            if order_exists:
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_nr__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            if check():
                 self._send_confirmation_email(order)
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
