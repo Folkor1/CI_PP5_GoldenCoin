5c9eedc6319870a0df964281cd09d2abb4dadf9a
Folkor1 <igorvasiljevs@gmail.com>
2023-01-28 16:56:27 +0000

    Add test for coins page view

--- coins/tests.py
+++ coins/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViewBag(TestCase):
+    """
+    Class to test coins page views
+    """
+
+    def test_render_bag_page(self):
+        """
+        Check if coins.html is rendered correctly
+        """
+
+        response = self.client.get('/coins/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'coins/coins.html')
