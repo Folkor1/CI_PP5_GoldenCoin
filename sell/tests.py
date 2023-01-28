e150f445a836a908933afb1ab0e0afdd1ab72f7d
Folkor1 <igorvasiljevs@gmail.com>
2023-01-28 17:48:10 +0000

    Add test for sell view

--- sell/tests.py
+++ sell/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViewSell(TestCase):
+    """
+    Class to test sell view
+    """
+
+    def test_render_sell_page(self):
+        """
+        Check if sell.html is rendered correctly
+        """
+
+        response = self.client.get('/sell/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sell/sell.html')
