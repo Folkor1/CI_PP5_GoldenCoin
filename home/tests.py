4828df72943817b1fb0d8329a3fac2d8f0396f04
Folkor1 <igorvasiljevs@gmail.com>
2023-01-28 17:10:39 +0000

    Add test for home view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViewHome(TestCase):
+    """
+    Class to test home view
+    """
+
+    def test_render_home_page(self):
+        """
+        Check if index.html is rendered correctly
+        """
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
